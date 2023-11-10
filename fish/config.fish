b4442667fd4395121f560f3a9387d832e396651c
nedjulius <julius.nedzinskas@gmail.com>
2023-11-09 23:36:09 -0500

    Add opam init

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# opam configuration
+source /Users/juliusned/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
